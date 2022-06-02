6169a96a3fe1bf9aa25bf1e0dbd9e757e17d57a3
Thorvald Ballestad <thorvalb@stud.ntnu.no>
2022-06-02 11:54:37 +0200

    Updates figures and inlcude final exr for typeii

--- presentation/.auctex-auto/presentation.el
+++ presentation/.auctex-auto/presentation.el
@@ -1,6 +1,8 @@
 (TeX-add-style-hook
  "presentation"
  (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("biblatex" "url=false" "doi=false")))
    (add-to-list 'LaTeX-verbatim-environments-local "semiverbatim")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
@@ -17,12 +19,16 @@
     "slashed"
     "braket"
     "subcaption"
+    "biblatex"
     "bm")
    (LaTeX-add-labels
     "eq:1"
     "eq:5"
     "eq:2"
     "eq:3"
-    "eq:4"))
+    "eq:4"
+    "eq:6")
+   (LaTeX-add-bibliographies
+    "Master"))
  :latex)
 
