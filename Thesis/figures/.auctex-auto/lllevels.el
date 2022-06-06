0087469d166be9b4a1a6b83d62e2672a190e325b
Thorvald Ballestad <thorvalb@stud.ntnu.no>
2022-06-06 15:38:29 +0200

    Adds rashba figure to project

---
+++ Thesis/figures/.auctex-auto/lllevels.el
@@ -0,0 +1,17 @@
+(TeX-add-style-hook
+ "lllevels"
+ (lambda ()
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (TeX-add-symbols
+    '("LLplot" 2)
+    "tz"
+    "myPlots"))
+ :latex)
+
